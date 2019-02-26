27e5094e6a9f057f54bebed9f85d5ce03fad26d3
samnejati <37846419+samnejati@users.noreply.github.com>
2019-02-25 19:43:02 -0700

    calculator V2.0
    Methods are functioning except getHistory ()

--- Calculator.java
+++ Calculator.java
@@ -1,39 +1,46 @@
 /**
-* This program contains a framework for a calulator. In version 1.0 all the methods are avilable but all are non-functioning.
-* This calculator program simulates a real calculator and has the main fuctions working as methods
+* This program contains a framework for a calulator. In version 2.0 all the methods are avilable and fucntioning except getHistory.
 *
 * @author Hesssam Nejati
-* @version 1.0
+* @version 2.0
 * @since 2019-02-23
 */
-package cse360assign2;
-
 public class Calculator {
 
 	private int total;
 
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
 	}
 
+	// The getTotal method should return the current total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 
+	// The add method adds the parameter to the total variable
 	public void add (int value) {
-
+		total = total + value;
 	}
 
+	// The subtract method subtracts the parameter from the total variable
 	public void subtract (int value) {
-
+		total = total - value;
 	}
 
+	// The multiply method multiplies the total by the parameter
 	public void multiply (int value) {
-
+		total = total * value;
 	}
 
+	// The divide method divides the total by the parameter. Uses integer division.
+	// If the parameter is zero, sets the total to zero. Does not print an error message or rasise an exception
 	public void divide (int value) {
-
+		if (value == 0){
+			total = 0;
+		} else{
+			total = total / value;
+		}
 	}
 
 	public String getHistory () {
