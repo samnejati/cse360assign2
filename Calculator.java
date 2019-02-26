a72eaeec44f4b89bbba983d8571686766ee695e0
samnejati <37846419+samnejati@users.noreply.github.com>
2019-02-25 19:10:57 -0700

    calculator V1.0
    Just the framework. Methods are not there

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+/**
+* This program contains a framework for a calulator. In version 1.0 all the methods are avilable but all are non-functioning.
+* This calculator program simulates a real calculator and has the main fuctions working as methods
+*
+* @author Hesssam Nejati
+* @version 1.0
+* @since 2019-02-23
+*/
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	public int getTotal () {
+		return 0;
+	}
+
+	public void add (int value) {
+
+	}
+
+	public void subtract (int value) {
+
+	}
+
+	public void multiply (int value) {
+
+	}
+
+	public void divide (int value) {
+
+	}
+
+	public String getHistory () {
+		return "";
+	}
+}
